9a5bdd2eb39b3d9fe9519edf345322ef2c357bce
sangyongchoi <csy7792@naver.com>
2021-04-26 21:23:17 +0900

    add @EnableWebFlux

--- demo/src/main/kotlin/com/webflux/demo/DemoApplication.kt
+++ demo/src/main/kotlin/com/webflux/demo/DemoApplication.kt
@@ -2,8 +2,10 @@ package com.webflux.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.reactive.config.EnableWebFlux
 
 @SpringBootApplication
+@EnableWebFlux
 class DemoApplication
 
 fun main(args: Array<String>) {
