46153698e7e81f3a22d8ab9af8b2bba99c370f50
sangyongchoi <csy7792@naver.com>
2021-04-26 19:06:02 +0900

    Annotation Example

---
+++ demo/src/main/kotlin/com/webflux/demo/DemoController.kt
@@ -0,0 +1,16 @@
+package com.webflux.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+@RestController
+class DemoController {
+
+    @GetMapping("/demo/mono")
+    fun monoDemo(): Mono<String> = Mono.just("demo")
+
+    @GetMapping("/demo/flux")
+    fun fluxDemo(): Flux<String> = Flux.just("demo1", "demo2", "demo3")
+}
